158453483bce599731e128e9ad3cef4209c17a54
Yevhenia.s.Krivko <yevhenia.s.krivko@outlook.com>
2016-02-16 14:13:21 +0200

    Added IdentityServices

--- BusinessServices.DomainLogic/Interfaces/Identity/IUserService.cs
+++ BusinessServices.DomainLogic/Interfaces/Identity/IUserService.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessServices.DomainLogic.Models;
+using System.Security.Claims;
 
 namespace BusinessServices.DomainLogic.Interfaces.Identity
 {
     public interface IUserService : IDisposable
     {
+        Task<OperationDetails> Create(User userDto);
 
+        Task<ClaimsIdentity> Authenticate(User userDto);
     }
 }
