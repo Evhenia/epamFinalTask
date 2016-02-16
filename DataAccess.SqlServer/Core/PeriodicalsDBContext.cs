80f227d578b5123dd7a2e9e19ca547a8d9cc856a
Yevhenia.s.Krivko <yevhenia.s.krivko@outlook.com>
2016-02-16 09:32:33 +0200

    Added DBContext

--- DataAccess.SqlServer/Core/PeriodicalsDBContext.cs
+++ DataAccess.SqlServer/Core/PeriodicalsDBContext.cs
@@ -6,6 +6,20 @@ namespace DataAccess.SqlServer.Core
 {
     public class PeriodicalsDBContext : IdentityDbContext<User>
     {
+        public PeriodicalsDBContext()
+            : base("PeriodicalsConnection")
+        {
+            Database.SetInitializer(new PeriodicalsDBInitializer());
+        }
 
+        public DbSet<Periodical> Periodicals { get; set; }
+
+        public DbSet<Subscription> Subscriptions { get; set; }
+        
+        public DbSet<Theme> Themes { get; set; }
+
+        public DbSet<MoneyAccount> MoneyAccounts { get; set; }
+
+        public DbSet<Payment> Payments { get; set; }
     }
 }
---
+++ DataAccess.SqlServer/Core/PeriodicalsDBInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.SqlServer.Core
+{
+    public class PeriodicalsDBInitializer : CreateDatabaseIfNotExists<PeriodicalsDBContext>
+    {
+        protected override void Seed(PeriodicalsDBContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
