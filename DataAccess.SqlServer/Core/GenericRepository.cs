8184d2918b11a788af25da80f5ac3c6c9ee89fa0
Yevhenia.s.Krivko <yevhenia.s.krivko@outlook.com>
2016-02-16 09:59:38 +0200

    Added GenericRepository

---
+++ DataAccess.SqlServer/Core/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.SqlServer.Interfaces;
+
+namespace DataAccess.SqlServer.Core
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly PeriodicalsDBContext _context;
+
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(PeriodicalsDBContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public virtual TEntity GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual TEntity GetFirstOrDefault(Func<TEntity, bool> predicate)
+        {
+            return _dbSet.FirstOrDefault(predicate);
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual bool Exist(int id)
+        {
+            return _dbSet.Find(id) != null;
+        }
+    }
+}
---
+++ DataAccess.SqlServer/Interfaces/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.SqlServer.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        TEntity GetFirstOrDefault(Func<TEntity, bool> predicate);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entityToUpdate);
+
+        void Delete(TEntity entityToDelete);
+
+        bool Exist(int id);
+    }
+}
